ec9ec6ba39b7ff85db670a372d13df4f684214ed
mahlamshrt298 <mahlamashrooti@gmail.com>
2026-02-15 20:41:13 +0330

    add temporary create admin view

--- accounts/urls.py
+++ accounts/urls.py
@@ -75,4 +75,6 @@ urlpatterns = [
     path("notifications/", views.notifications_list, name="notifications_list"),
     path('ajax/add_review/<int:appointment_id>/', views.ajax_add_review, name='ajax_add_review'),
     path('cancel_appointment/<int:appt_id>/', views.cancel_appointment, name='cancel_appointment'),
+    
+    path("create-admin/", views.create_admin,name="create_admin"),
     ]
