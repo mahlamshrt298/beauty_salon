d6f7a71528a7acb0bdf2f2ebd601a6943c18ec54
mahlamshrt298 <mahlamashrooti@gmail.com>
2026-02-19 21:35:47 +0330

    new

--- beauty_salon/settings.py
+++ beauty_salon/settings.py
@@ -202,6 +202,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
